9eb0a65e224f8d04c5a0feff4b457ddf47224d37
epsxy <epsxy@users.noreply.github.com>
2018-04-03 22:48:25 +0200

    Code reformatting

--- clean.js
+++ clean.js
@@ -2,71 +2,71 @@ var path = require('path');
 var program = require('commander');
 var pkg = require(path.join(__dirname, 'package.json'));
 var logger = require('winston');
-var discord = require("discord.js");
+var discord = require('discord.js');
 var auth = require(path.join(__dirname, 'auth.json'));
 var helpers = require('./helpers');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-  colorize: true
+    colorize: true
 });
 logger.level = 'debug';
 
 program
-  .version(pkg.version)
-  .option('-g, --guild <guild>', 'Discord guild', '')
-  .option('-c, --channel <channel>', 'Discord channel', '')
-  .option('-t, --token <token>', 'Discord token', '')
-  .parse(process.argv);
+    .version(pkg.version)
+    .option('-g, --guild <guild>', 'Discord guild', '')
+    .option('-c, --channel <channel>', 'Discord channel', '')
+    .option('-t, --token <token>', 'Discord token', '')
+    .parse(process.argv);
 
 const client = new discord.Client();
 
 var token = undefined
 if (program.token !== '' && program.token !== undefined) {
-  token = program.token;
+    token = program.token;
 } else if (auth.token !== undefined) {
-  token = auth.token;
+    token = auth.token;
 } else {
-  logger.error('Needs a user token: provided with --token option or in auth.json');
-  client.destroy();
+    logger.error('Needs a user token: provided with --token option or in auth.json');
+    client.destroy();
 }
 
 client.login(token);
 
-client.on('ready', function (evt) {
-  logger.info('Connected successfully as — ' + client.user.username);
-  if (program.guild === '') {
-    logger.error('Not implemented yet — you should provide both a guild and a channel name')
-  } else if (program.channel === '') {
-    logger.error('Not implemented yet — you should provide both a guild and a channel name')
-  } else {
-    logger.info(`Will delete all messages in: ${program.guild} — ${program.channel}`);
-    channelsMatchingProvidedName = helpers.getChannelFromGuild(client, program.guild, program.channel)
-    if (channelsMatchingProvidedName.length == 0) {
-      logger.error(`No channel is matching the provided name: ${program.channel}`);
-      client.destroy();
-    } else if (channelsMatchingProvidedName.length == 1) {
-      logger.info(`Deleting messages from channel ${program.channel}`);
+client.on('ready', function(evt) {
+    logger.info(`Connected successfully as — ${client.user.username}`);
+    if (program.guild === '') {
+        logger.error('Not implemented yet — you should provide both a guild and a channel name')
+    } else if (program.channel === '') {
+        logger.error('Not implemented yet — you should provide both a guild and a channel name')
     } else {
-      logger.info('Several channels are matching the provided name, messages in all channels will be deleted');
+        logger.info(`Will delete all messages in: ${program.guild} — ${program.channel}`);
+        channelsMatchingProvidedName = helpers.getChannelFromGuild(client, program.guild, program.channel)
+        if (channelsMatchingProvidedName.length == 0) {
+            logger.error(`No channel is matching the provided name: ${program.channel}`);
+            client.destroy();
+        } else if (channelsMatchingProvidedName.length == 1) {
+            logger.info(`Deleting messages from channel ${program.channel}`);
+        } else {
+            logger.info('Several channels are matching the provided name, messages in all channels will be deleted');
+        }
+        helpers.deleteMessages(channelsMatchingProvidedName, client.user.username)
+            .then(messagesCountArr => {
+                total = messagesCountArr.reduce((total, value) => total + value);
+                logger.info(`Deletion finished`)
+                client.destroy();
+            }).catch(err => logger.error(err));
     }
-    helpers.deleteMessages(channelsMatchingProvidedName, client.user.username)
-      .then(messagesCountArr => {
-        total = messagesCountArr.reduce((total, value) => total + value);
-        logger.info(`Deletion finished`)
-        client.destroy();
-      }).catch(err => logger.error(err));
-  }
 
 
 });
 
-client.on('disconnect', function (evt) {
-  if (evt.code !== 1000) {
-    logger.error('Error: ' + evt.reason);
-  } else {
-    logger.info("Bye");
-  }
-  process.exit(0);
+client.on('disconnect', function(evt) {
+    if (evt.code !== 1000) {
+        logger.error(`Error: ${evt.reason}`);
+    } else {
+        logger.info('Bye');
+    }
+    process.exit(0);
 });
--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var logger = require('winston');
 var exports = module.exports = {};
@@ -6,37 +6,37 @@ var exports = module.exports = {};
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-  colorize: true
+    colorize: true
 });
 logger.level = 'debug';
 
 
 class DeletionReport {
-  constructor(channel_id, actual, wanted) {
-    this.channel_id = channel_id;
-    this.actual = actual;
-    this.wanted = wanted;
-  }
-  perCent() {
-    if (this.wanted !== 0) {
-      return Math.round((this.actual / this.wanted) * 100);
-    } else {
-      return 0;
+    constructor(channel_id, actual, wanted) {
+        this.channel_id = channel_id;
+        this.actual = actual;
+        this.wanted = wanted;
     }
-  };
+    perCent() {
+        if (this.wanted !== 0) {
+            return Math.round((this.actual / this.wanted) * 100);
+        } else {
+            return 0;
+        }
+    };
 }
 
 /**
  * 
  * @param {Client} client 
  */
-exports.getAllChannels = function (client) {
-  var guilds = client.guilds;
-  var channelsByGuilds = {};
-  guilds.forEach(function (guild) {
-    channelsByGuilds[guild.id] = guild.channels;
-  });
-  return guilds;
+exports.getAllChannels = function(client) {
+    var guilds = client.guilds;
+    var channelsByGuilds = {};
+    guilds.forEach(function(guild) {
+        channelsByGuilds[guild.id] = guild.channels;
+    });
+    return guilds;
 };
 
 /**
@@ -44,14 +44,14 @@ exports.getAllChannels = function (client) {
  * @param {Client} client 
  * @param {String} specifiedGuild 
  */
-exports.getAllChannelsGuild = function (client, specifiedGuild) {
-  var channels = [];
-  client.guilds.forEach(function (guild) {
-    if (guild.name === specifiedGuild) {
-      channels = guild.channels;
-    }
-  });
-  return channels;
+exports.getAllChannelsGuild = function(client, specifiedGuild) {
+    var channels = [];
+    client.guilds.forEach(function(guild) {
+        if (guild.name === specifiedGuild) {
+            channels = guild.channels;
+        }
+    });
+    return channels;
 };
 
 /**
@@ -60,18 +60,18 @@ exports.getAllChannelsGuild = function (client, specifiedGuild) {
  * @param {String} specifiedGuild 
  * @param {String} specifiedChannel 
  */
-exports.getChannelFromGuild = function (client, specifiedGuild, specifiedChannel) {
-  var channels = [];
-  client.guilds.forEach(function (g) {
-    if (g.name === specifiedGuild) {
-      g.channels.forEach(function (c) {
-        if (c.name == specifiedChannel) {
-          channels.push(c);
+exports.getChannelFromGuild = function(client, specifiedGuild, specifiedChannel) {
+    var channels = [];
+    client.guilds.forEach(function(g) {
+        if (g.name === specifiedGuild) {
+            g.channels.forEach(function(c) {
+                if (c.name == specifiedChannel) {
+                    channels.push(c);
+                }
+            });
         }
-      });
-    }
-  });
-  return channels;
+    });
+    return channels;
 };
 
 /**
@@ -81,40 +81,40 @@ exports.getChannelFromGuild = function (client, specifiedGuild, specifiedChannel
  * @param {Message} messageBefore
  * @return {Message[]} Messages of connected user for channel
  */
-exports.fetchMessages = function (textChannel, username, messageBefore, lengthBefore) {
-  var limit = 100;
-  var allMessages = [];
-  var lastMsg;
-  return new Promise((resolve, reject) => {
-    textChannel.fetchMessages({ limit: limit, before: messageBefore != undefined ? messageBefore : null })
-      .then((messages) => {
-        messages.map((msg) => {
-          if (msg.author.username === username) {
-            allMessages.push(msg);
-          }
-        });
-        //NB: Don't use .length but .size on messages!
-        var currentTotal = (lengthBefore != undefined ? lengthBefore : 0) + messages.size
-        logger.debug(`Fetching channel messages ... ${currentTotal}`)
-        if (messages.size == limit) {
-          this.fetchMessages(textChannel, username, messages.lastKey(), currentTotal)
+exports.fetchMessages = function(textChannel, username, messageBefore, lengthBefore) {
+    var limit = 100;
+    var allMessages = [];
+    var lastMsg;
+    return new Promise((resolve, reject) => {
+        textChannel.fetchMessages({ limit: limit, before: messageBefore != undefined ? messageBefore : null })
             .then((messages) => {
-              allMessages = allMessages.concat(messages);
-              resolve(allMessages);
+                messages.map((msg) => {
+                    if (msg.author.username === username) {
+                        allMessages.push(msg);
+                    }
+                });
+                //NB: Don't use .length but .size on messages!
+                var currentTotal = (lengthBefore != undefined ? lengthBefore : 0) + messages.size
+                logger.debug(`Fetching channel messages ... ${currentTotal}`)
+                if (messages.size == limit) {
+                    this.fetchMessages(textChannel, username, messages.lastKey(), currentTotal)
+                        .then((messages) => {
+                            allMessages = allMessages.concat(messages);
+                            resolve(allMessages);
+                        })
+                        .catch((err) => {
+                            logger.error(err);
+                            reject(err);
+                        });
+                } else {
+                    resolve(allMessages);
+                }
             })
             .catch((err) => {
-              logger.error(err);
-              reject(err);
+                logger.error(err);
+                reject(err);
             });
-        } else {
-          resolve(allMessages);
-        }
-      })
-      .catch((err) => {
-        logger.error(err);
-        reject(err);
-      });
-  });
+    });
 };
 
 /**
@@ -123,31 +123,31 @@ exports.fetchMessages = function (textChannel, username, messageBefore, lengthBe
  * @param {String} username
  * @returns {DeletionReport}
  */
-exports.deleteMsgForChannel = function (channel, username) {
-  return new Promise((resolve, reject) => {
-    this.fetchMessages(channel, username).then((messagesToBeDeleted) => {
-      logger.info(`${messagesToBeDeleted.length} messages to delete`);
-      var allPromises = Promise.all(messagesToBeDeleted.map(msg => new Promise((resolve, reject) => {
-        msg.delete().then(res => {
-          logger.info(`Message ${res.id} -> Deleted`);
-          resolve(res);
+exports.deleteMsgForChannel = function(channel, username) {
+    return new Promise((resolve, reject) => {
+        this.fetchMessages(channel, username).then((messagesToBeDeleted) => {
+            logger.info(`${messagesToBeDeleted.length} messages to delete`);
+            var allPromises = Promise.all(messagesToBeDeleted.map(msg => new Promise((resolve, reject) => {
+                msg.delete().then(res => {
+                    logger.info(`Message ${res.id} -> Deleted`);
+                    resolve(res);
+                }).catch((err) => {
+                    logger.error(err);
+                    resolve(undefined)
+                });
+            })));
+            allPromises.then(deletedMessages => {
+                var filteredResults = deletedMessages.filter(msg => msg !== undefined);
+                resolve(new DeletionReport(channel.id, filteredResults.length, messagesToBeDeleted.length));
+            }).catch((err) => {
+                logger.error(err.message);
+                reject(err);
+            });
         }).catch((err) => {
-          logger.error(err);
-          resolve(undefined)
+            logger.error(err);
+            reject(err);
         });
-      })));
-      allPromises.then(deletedMessages => {
-        var filteredResults = deletedMessages.filter(msg => msg !== undefined);
-        resolve(new DeletionReport(channel.id, filteredResults.length, messagesToBeDeleted.length));
-      }).catch((err) => {
-        logger.error(err.message);
-        reject(err);
-      });
-    }).catch((err) => {
-      logger.error(err);
-      reject(err);
     });
-  });
 }
 
 /**
@@ -156,21 +156,21 @@ exports.deleteMsgForChannel = function (channel, username) {
  * @param {String} username
  * @returns {DeletionReport[]}
  */
-exports.deleteMessages = function (textChannels, username) {
-  return new Promise((resolve, reject) => {
-    var deletions = Promise.all(textChannels.map(chan => this.deleteMsgForChannel(chan, username)));
-    deletions.then(reports => {
-      reports.map(report => {
-        if (report.wanted !== 0) {
-          logger.info(`Channel: ${report.channel_id} -> ${report.actual}/${report.wanted} messages deleted (${report.perCent()}%)`);
-        } else {
-          logger.info(`Channel: ${report.channel_id} -> No message to delete`);
-        }
-      });
-      resolve(reports);
-    }).catch((err) => {
-      logger.error(err);
-      reject(err);
+exports.deleteMessages = function(textChannels, username) {
+    return new Promise((resolve, reject) => {
+        var deletions = Promise.all(textChannels.map(chan => this.deleteMsgForChannel(chan, username)));
+        deletions.then(reports => {
+            reports.map(report => {
+                if (report.wanted !== 0) {
+                    logger.info(`Channel: ${report.channel_id} -> ${report.actual}/${report.wanted} messages deleted (${report.perCent()}%)`);
+                } else {
+                    logger.info(`Channel: ${report.channel_id} -> No message to delete`);
+                }
+            });
+            resolve(reports);
+        }).catch((err) => {
+            logger.error(err);
+            reject(err);
+        });
     });
-  });
 }
